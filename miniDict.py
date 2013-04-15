61ea0897ec07ae4842da1960744bee36293f6e7d
HankZhou <z15804268950@icloud.com>
2013-04-15 21:10:12 +0800

    a

--- miniDict.py
+++ miniDict.py
@@ -3,21 +3,38 @@
 
 import requests
 import json
-import urllib2
-import urllib
-#import simplejson as json
 import sys
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
+
+"""
+-------------------------------80size-------------------------------------------
+"""
+
+def cmdPr(explain):
+	trans=""
+	print("音标: [ %s ]" % explain['basic']['phonetic'])
+	for i in explain['basic']['explains']:
+		print(i)
+
+def key():
+	key = '258070906'
+	keyfrom = 'commandtranslation'
+	keyArray = [ key , keyfrom ]
+	return keyArray
+
+def getExplain(keyArray):
+	word=sys.argv[1]
+        url='http://fanyi.youdao.com/openapi.do?keyfrom=' + keyArray[1] +'&key=' + \
+                 keyArray[0] + '&type=data&doctype=json&version=1.1&q=' + word
+        transJson = requests.get(url).text
+	explain = json.loads(transJson)
+	return explain
+	
 
 if __name__ == "__main__":
-	key='258070906'
-	keyfrom='commandtranslation'
-	word='translations'
-	url='http://fanyi.youdao.com/openapi.do?keyfrom=' +keyfrom +'&key=' + key + '&type=data&doctype=json&version=1.1&q=' + word
-	print(url)
-	transJson = requests.get(url)
-	print(transJson.text)
-#	request=urllib2.urlopen(url)
-#	data=request.read()
-#	print(data)
+	keyArray = key()
+	explain = getExplain(keyArray)
+	cmdPr(explain)
